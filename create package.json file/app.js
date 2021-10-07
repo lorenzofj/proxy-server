1df7ac13a2cebe26e5e067b6ad0c06641c0a1085
lorenzofj <lorenzofran1@gmail.com>
2021-10-07 09:36:51 -0300

    installing npm packages
    From npmjs.com. How to install and uninstall packages.

--- create package.json file/app.js
+++ create package.json file/app.js
@@ -2,3 +2,11 @@
 El archivo package.json es el responsable de almacenar toda la metadata de un proyecto (nombre del proyecto, versión, etc).
 Escribir en consola npm init
 */
+
+/*
+Para instalar un paquete se puede ir a la página www.npmjs.com y luego escribir en consola "npm install (nombre del paquete)".
+Por ejemplo: npm install express.
+Para desinstalar un paquete escribir en consola "npm uninstall (nombre del paquete)".
+*/
+
+//const ex = require('express');
