1527f67c61c4f9eb45a20c03923c51f8dfd09b63
lorenzofj <lorenzofran1@gmail.com>
2021-10-07 10:21:56 -0300

    semantic versioning

--- create package.json file/app.js
+++ create package.json file/app.js
@@ -9,4 +9,12 @@ Por ejemplo: npm install express.
 Para desinstalar un paquete escribir en consola "npm uninstall (nombre del paquete)".
 */
 
-//const ex = require('express');
+/*
+En el archivo package.json se encuentran las dependencies que son los paquetes instalados en el proyecto.
+Esto muestra la versión y cada número significa: (1.7.18) => versión mayor.versión menor.parche
+
+El símbolo ^ establece que si sale una nueva versión mayor no actualice automaticamente el paquete ya instalado
+pero si hay una nueva versión menor o parche si se actualiza.
+Si se pone el símbolo ~ se establece que se pueden hacer solo actualizaciones de parche.
+Si no se pone ningún símbolo no se realizan actualizaciones y se mantiene la misma versión siempre.
+*/
