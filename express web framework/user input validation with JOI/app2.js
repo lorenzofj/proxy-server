acc45c378c2f7b914a09b919403e413902b3f0ae
lorenzofj <lorenzofran1@gmail.com>
2021-10-21 10:56:00 -0300

    validate objects with joi

--- express web framework/user input validation with JOI/app2.js
+++ express web framework/user input validation with JOI/app2.js
@@ -1,3 +1,4 @@
+const { string, preferences } = require('joi');
 const Joi = require('joi');
 
 const arrayString = ['banana', 'bacon', 'beer'];
@@ -11,3 +12,27 @@ const userInput = {
     },
     preferences: arrayString
 };
+
+const personalInfoSchema = Joi.object({
+    streetAddress : Joi.string().trim().required(),
+    city : Joi.string().trim().required(),
+    state : Joi.string().trim().required()
+});
+
+const preferencesSchema = Joi.array().items(Joi.string()); //items toma como argumento el esquema que queremos, en este caso un array de strings
+
+//Juntamos los 2 esquemas anteriores en uno solo
+const schema = Joi.object({
+    personalInfo : personalInfoSchema,
+    preferences : preferencesSchema
+});
+
+Joi.assert(userInput, schema, (error, result) => {
+    if(error) console.log(error);
+    else console.log(result);
+});
+
+/*schema.validate(userInput, schema, (error, result) => {
+    if(error) console.log(error);
+    else console.log(result);
+});*/
