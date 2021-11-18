7b27066e6c53c25049f11e773c289cad64acc07e
Matias <lorenzofran1@gmail.com>
2021-11-18 12:43:13 -0300

    final details
    - added functionality to next button
    - added author, title and full size image link.

--- NASA APIs/public/js/app.js
+++ NASA APIs/public/js/app.js
@@ -4,20 +4,26 @@ const previousDate = document.querySelector('#previous');
 const nextDate = document.querySelector('#next');
 const datePicker = document.querySelector('#date');
 const imageHeading = document.querySelector('.imageHeading');
-const videoByNasa = document.querySelector('.videoByNasa');
 const explanation = document.querySelector('#explanation');
+const copyright = document.querySelector('#copyright');
+const title = document.querySelector('#title');
+const fullSizeImage = document.querySelector('#fullSizeImage');
 
-const getFormatedDate = (date) => {
-    let dd = date.getDate();
-    let mm = date.getMonth() + 1; //Enero es 0
-    let yyyy = date.getFullYear();
-    if (dd < 10) {
-        dd = '0' + dd
+//Format Date object that goes to the datepicker
+const getFormatedDate = (date, isNext) => {
+    let day = 0;
+    if(isNext){
+        day = date.getDate() + 2;
     }
-    if (mm < 10) {
-        mm = '0' + mm
+    else{
+        day = date.getDate();
     }
-    return date = yyyy + '-' + mm + '-' + dd;
+    let month = date.getMonth() + 1; //January is 0
+    let year = date.getFullYear();
+
+    if (day < 10) day = '0' + day;
+    if (month < 10) month = '0' + month;
+    return date = year + '-' + month + '-' + day;
 }
 
 //Set todays value to date input and max attribute
@@ -25,6 +31,18 @@ let today = new Date();
 today = getFormatedDate(today);
 datePicker.setAttribute("value", today);
 datePicker.setAttribute("max", today);
+
+//Disable Next button if its todays date
+const isTodaysDate = () => {
+    let todaysDate = new Date();
+    todaysDate = getFormatedDate(todaysDate);
+    if (datePicker.value == todaysDate) {
+        document.getElementById("btn-next").setAttribute("disabled", true);
+        return true;
+    }
+    else return false;
+};
+
 
 //When page loads get todays image and displays it
 window.addEventListener("load", function(){
@@ -49,11 +67,13 @@ info.addEventListener('submit', (error) => {
     fetchInfo(data);
 });
 
+//Add event listener to next and previous buttons
 previousDate.addEventListener('submit', (error) => {
     error.preventDefault();
 
+    const isNext = false;
     let previousDate = new Date(datePicker.value);
-    const date = getFormatedDate(previousDate);
+    const date = getFormatedDate(previousDate, isNext);
 
     const data = {
         date
@@ -66,6 +86,22 @@ previousDate.addEventListener('submit', (error) => {
 nextDate.addEventListener('submit', (error) => {
     error.preventDefault();
     
+    const isNext = true;
+    let nextDate = new Date(datePicker.value);
+    const date = getFormatedDate(nextDate, isNext);
+
+    if(isTodaysDate()){
+        explanation.innerHTML = "Wait until tomorrow for the next image/video!";
+        document.getElementById("box").innerHTML = "No image or video";
+    }
+    else{
+        const data = {
+            date
+        }
+    
+        fetchInfo(data);
+        datePicker.value = date;
+    }   
 });
 
 const fetchInfo = (data) => {
@@ -81,14 +117,19 @@ const fetchInfo = (data) => {
                         document.getElementById("box").innerHTML = `<p>${data.error}</p>`;
                     }
                     else {
+                        title.innerHTML = `Title: ${data.title}`;
                         explanation.innerHTML = data.explanation;
+                        if(data.copyright != undefined)copyright.innerHTML = `Author (copyright): ${data.copyright}`;                       
+                        fullSizeImage.innerHTML = `See full size image <a href="${data.url}" target="_blank" rel="noopener noreferrer">here</a>.`;
                         if(data.url.indexOf('youtube') > -1){
                             document.getElementById("box").innerHTML = `<iframe class="videoByNasa" width="600" height="400" src=${data.url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+                            document.getElementById("btn-next").removeAttribute("disabled");
                             if(datePicker.value != today) imageHeading.textContent = "Video from " + datePicker.value;
                             else imageHeading.textContent = "Todays image/video:";
                         }
                         else{
                             document.getElementById("box").innerHTML = `<img class="imageByNasa" src=${data.url} width="500" height="600">`;
+                            document.getElementById("btn-next").removeAttribute("disabled");
                             if(datePicker.value != today) imageHeading.textContent = "Image from " + datePicker.value;
                             else imageHeading.textContent = "Todays image/video:";                       
                         }                       
